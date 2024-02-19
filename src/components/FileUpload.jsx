243afd89e0b5cf982b631efea75cb5635651dcc9
Mubashir <mubasirahmad0305@gamil.com>
2024-02-20 03:42:37 +0530

    choose fikle chjanged

--- src/components/FileUpload.jsx
+++ src/components/FileUpload.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const FileUpload = ({ onFileChange }) => {
+    const fileInputRef = useRef(null);
+
     const handleFileChange = (event) => {
         const file = event.target.files[0];
         onFileChange(file);
     }
 
+    const handleButtonClick = () => {
+        fileInputRef.current.click();
+    }
+
     return (
         <div className="container1-md">
             <h2>Upload</h2>
-            <input type="file" accept=".mp3" multiple onChange={handleFileChange} /><br />
+            <input
+                type="file"
+                ref={fileInputRef}
+                accept=".mp3"
+                multiple
+                onChange={handleFileChange}
+                style={{ display: 'none' }} // Hide the default file input
+            />
+            <button type="button" class="btn btn-primary"  onClick={handleButtonClick}>Choose File</button>
         </div>
     );
 }
